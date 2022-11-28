9792be895440787be2524c0b5df07dd3c7fc877c
Universenn <dnwndhks98@gmail.com>
2022-11-28 15:20:22 +0900

    RestControllerAdvice 적용

---
+++ src/main/java/com/example/jpapractice/exception/HospitalReviewAppException.java
@@ -0,0 +1,17 @@
+package com.example.jpapractice.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.web.server.ResponseStatusException;
+@Getter
+@AllArgsConstructor
+public class HospitalReviewAppException extends RuntimeException{
+    private ErrorCode errorCode;
+    private String message;
+
+    @Override
+    public String toString() {
+        if(message == null) return errorCode.getMessage();
+        return String.format("%s, %s", errorCode.getMessage(),message);
+    }
+}
--- src/main/java/com/example/jpapractice/service/UserService.java
+++ src/main/java/com/example/jpapractice/service/UserService.java
@@ -3,9 +3,13 @@ package com.example.jpapractice.service;
 import com.example.jpapractice.domain.User;
 import com.example.jpapractice.domain.UserDto;
 import com.example.jpapractice.domain.UserJoinRequest;
+import com.example.jpapractice.exception.ErrorCode;
+import com.example.jpapractice.exception.HospitalReviewAppException;
 import com.example.jpapractice.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import static com.example.jpapractice.exception.ErrorCode.*;
 
 @Service
 @RequiredArgsConstructor
@@ -19,7 +23,10 @@ public class UserService {
         // orElseThrow 없을때 에러 사용
         // userRepository.findByUserName(request.getUserName()).orElseThrow(()-> new RuntimeException("사용중인 이름입니다."));
         // ifPresent 없을때 에러 사용
-        userRepository.findByUserName(request.getUserName()).ifPresent(user ->{throw new RuntimeException("현재 사용중인 이름입니다.");});
+        userRepository.findByUserName(request.getUserName())
+                .ifPresent(user -> {
+                    throw new HospitalReviewAppException(DUPLICATED_USER_NAME, String.format("UserName : %s", request.getUserName()));
+                });
         //  회원가입 save
         User savedUser = userRepository.save(request.toEntity());
 
